71521a32cf5aa69c325e0f9b6f1c44bb993e25b6
“Gian1984” <gl.tiengo@gmail.com>
2020-12-14 23:45:44 +0100

    adding some style & some field to the api

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // fetch('https://corsed.herokuapp.com')
 //     .then(response => response.json())
 //     .then(element => { console.log(element) });
+
 document.getElementById("submit").addEventListener("click", priceValutaion);
 
 let _data = {};
@@ -11,12 +12,25 @@ function priceValutaion() {
     let area = parseInt(document.getElementById('area').value);
     let roomNumber = parseInt(document.getElementById('roomNumber').value);
     let zipCode = parseInt(document.getElementById('zipCode').value);
+    // let garden = document.getElementById('garden').value;
+    let gardenArea = parseInt(document.getElementById('gardenArea').value);
+    // let terrace = document.getElementById('terrace').value;
+    let terraceArea = parseInt(document.getElementById('terraceArea').value);
+    let facadesNumbers = parseInt(document.getElementById('facadesNumbers').value);
+    let buildingState = document.getElementById('buildingState').value;
+
     let _data = {
         data: {
             "property-type": property,
             "area": area,
             "rooms-number": roomNumber,
             "zip-code": zipCode,
+            // "garden": garden,
+            "garden-area": gardenArea,
+            // "terrace": terrace,
+            "terrace-area": terraceArea,
+            "facades-number": facadesNumbers,
+            "building-state": buildingState
         }
 
     };
